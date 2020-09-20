7c171e91605c6f0054bbfcaebf30daaae8b8248f
Kerem Yiğit <keremyigit87@gmail.com>
2020-09-20 15:19:27 +0300

    teste

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,18 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="manifest" href="manifest.json" />
-          <meta name="theme-color" content="#000000"/>
-          <link rel="apple-touch-icon" href="/icons/android-icon-192x192.png" />
           <meta name="robots" content="noindex"/>
+
+          <meta name="theme-color" content="#000000"/>
+          <meta name='msapplication-TileColor' content='#2B5797' />
+          <link rel="apple-touch-icon" href="/icons/android-icon-192x192.png" />
+
+          <meta name='application-name' content='PWA App' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='PWA App' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
         </Head>
         <body>
           <Main />
