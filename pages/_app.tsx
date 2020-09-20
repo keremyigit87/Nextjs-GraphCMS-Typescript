c51bc0a9e0e467017f65d77f024ebf8786d56561
Kerem Yiğit <keremyigit87@gmail.com>
2020-09-20 19:37:37 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import StoreContext from '../context/store';
 import { useState, useEffect } from 'react'
+import Head  from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   const [ isActive, setIsActive ] = useState(false);
@@ -13,6 +14,21 @@ function MyApp({ Component, pageProps }) {
       isActive, toggleActive,
     }}>
       <DefaultSeo {...SEO}/>
+        <Head>
+          <link rel="manifest" href="manifest.json" />
+          <meta name="robots" content="noindex"/>
+
+          <meta name="theme-color" content="#000000"/>
+          <meta name='msapplication-TileColor' content='#2B5797' />
+          <link rel="apple-touch-icon" href="/icons/android-icon-192x192.png" />
+
+          <meta name='application-name' content='PWA App' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='PWA App' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+        </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
