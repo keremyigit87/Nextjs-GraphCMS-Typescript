5fc3da993a3e96f79861722e4f2655ee2219cc07
Kerem Yiğit <keremyigit87@gmail.com>
2020-09-20 19:41:22 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,21 +14,6 @@ function MyApp({ Component, pageProps }) {
       isActive, toggleActive,
     }}>
       <DefaultSeo {...SEO}/>
-        <Head>
-          <link rel="manifest" href="manifest.json" />
-          <meta name="robots" content="noindex"/>
-
-          <meta name="theme-color" content="#000000"/>
-          <meta name='msapplication-TileColor' content='#2B5797' />
-          <link rel="apple-touch-icon" href="/icons/android-icon-192x192.png" />
-
-          <meta name='application-name' content='PWA App' />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='PWA App' />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-        </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
