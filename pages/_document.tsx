24e2978eeb6a1611367963788ccdbe6d3f772c16
Kerem Yiğit <keremyigit87@gmail.com>
2020-09-20 19:45:12 +0300

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="manifest" href="manifest.json" />
+          <link rel="manifest" href="/manifest.json" />
           <meta name="robots" content="noindex"/>
 
           <meta name="theme-color" content="#000000"/>
