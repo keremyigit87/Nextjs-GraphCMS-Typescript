d91742b6e51ed228aa2f399a8bea4da035f2d0a0
Kerem Yiğit <keremyigit87@gmail.com>
2020-12-08 12:59:48 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home({ }) {
           <div className="col-6 col-xs-5 col-sm-4 col-md-3 col-lg-2 col-xl-1"><div className="box"></div></div>
           <div className="col-6 col-xs-5 col-sm-4 col-md-3 col-lg-2 col-xl-1"><div className="box"></div></div>
         </div>
+        test
       </div>
     </main>
   )
